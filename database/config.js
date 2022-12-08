0aa6158d312b343b095961a311ddff2daa9f6353
rich27a <freeazul25@outlook.es>
2022-12-07 22:03:41 -0600

    pool added to config

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 require("dotenv").config();
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.DB_PASSWORD,
